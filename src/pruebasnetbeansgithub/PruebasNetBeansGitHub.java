ac3309d0dc81d0e224ebdb32a073f8984fd3a381
esolorzanp <esolorzanop@estudiante.uniajc.edu.co>
2024-05-20 23:23:32 -0500

    Mi segundo commit

--- src/pruebasnetbeansgithub/PruebasNetBeansGitHub.java
+++ src/pruebasnetbeansgithub/PruebasNetBeansGitHub.java
@@ -14,7 +14,14 @@ public class PruebasNetBeansGitHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo desde PruebasNetBeansGitHub");
-    }
+        System.out.println("Hola equipo desde PruebasNetBeansGitHub");
+        System.out.println("Esta linea es para segundo commit ");    
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            
+        }
     
+    }
+
 }
